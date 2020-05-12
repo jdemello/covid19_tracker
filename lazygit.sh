862c77334b27289c16abfa58dc958a21c135b11f
José De Mello <jdemello@uottawa.ca>
2020-05-12 16:44:43 -0400

    data update

--- lazygit.sh
+++ lazygit.sh
@@ -1,7 +1,7 @@
 function lazygit() {
   git add .
   git commit -a -m "$1"
-  git push origin https://github.com/jdemello/covid19_tracker.git
+  git push origin git@github.com:jdemello/covid19_tracker.git
 }
 
 lazygit "data update"
